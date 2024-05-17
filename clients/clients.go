1ff0b47534806c389b3c66dcd04baaba8fc68a72
bunjo <nemanja.pokornic02@gmail.com>
2024-05-17 10:33:59 +0200

    initialized clients

--- clients/clients.go
+++ clients/clients.go
@@ -1,44 +1,17 @@
 package clients
 
-import (
-	kuiperapi "github.com/c12s/kuiper/pkg/api"
-	magnetarapi "github.com/c12s/magnetar/pkg/api"
-	oortapi "github.com/c12s/oort/pkg/api"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
-	"log"
-)
+type Config struct {
+	Magnetar string
+	Kuiper   string
+	Oort     string
+}
 
-var Magnetar magnetarapi.MagnetarClient
-var Kuiper kuiperapi.KuiperClient
-var OortAdministrator oortapi.OortAdministratorClient
+var Clients Config
 
 func Init() {
-	Magnetar = newMagnetar("localhost:5000")
-	Kuiper = newKuiper("localhost:9001")
-	OortAdministrator = newOortAdministrator("localhost:8000")
-}
-
-func newMagnetar(address string) magnetarapi.MagnetarClient {
-	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		log.Fatalln(err)
+	Clients = Config{
+		Magnetar: "http://localhost:5555",
+		Kuiper:   "http://localhost:9001",
+		Oort:     "http://localhost:8000",
 	}
-	return magnetarapi.NewMagnetarClient(conn)
-}
-
-func newKuiper(address string) kuiperapi.KuiperClient {
-	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		log.Fatalln(err)
-	}
-	return kuiperapi.NewKuiperClient(conn)
-}
-
-func newOortAdministrator(address string) oortapi.OortAdministratorClient {
-	conn, err := grpc.Dial(address, grpc.WithTransportCredentials(insecure.NewCredentials()))
-	if err != nil {
-		log.Fatalln(err)
-	}
-	return oortapi.NewOortAdministratorClient(conn)
 }
